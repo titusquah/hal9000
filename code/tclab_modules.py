ab61218588e49a889b90aca4521f96f1f4cbcf2e
titusquah <46580668+titusquah@users.noreply.github.com>
2021-02-18 17:23:42 -0700

    updated modules and runner

--- code/tclab_modules.py
+++ code/tclab_modules.py
@@ -19,23 +19,41 @@ import pandas as pd
 
 def fan_cooling(mini_dpin1, mini_heater_board, temp_sp):
     print("Starting cooling procedure")
+    mini_heater_board.Q1(0)
+    mini_heater_board.Q2(0)
     current_temp = mini_heater_board.T1
     mini_dpin1.write(1)
+    start_time = time.time()
+    prev_time = start_time
+    sleep_max = 1
+    times, temps, heater_pwms, fan_pwms = [], [], [], []
     while current_temp > temp_sp - 1:
+        sleep = sleep_max - (time.time() - prev_time)
+        if sleep >= 0.01:
+            time.sleep(sleep - 0.01)
+        else:
+            time.sleep(0.01)
+        t = time.time()
+        prev_time = t
+        times.append(t - start_time)
         current_temp = mini_heater_board.T1
-        time.sleep(1)
+        temps.append(current_temp)
+        heater_pwms.append(0)
+        fan_pwms.append(1)
     print("Ending cooling procedure, current T = {0} °C".format(current_temp))
     mini_dpin1.write(0)
-    return None
+    return times, temps, heater_pwms, fan_pwms
 
 
-def set_initial_temp(mini_heater_board,
+def set_initial_temp(mini_dpin1,
+                     mini_heater_board,
                      temp_sp,
                      tol,
                      hold_time,
                      file_path=None):
     print("Setting initial temperature to {0} °C".format(temp_sp))
     stable = False
+    mini_dpin1.write(0)
     start_time = time.time()
     prev_time = start_time
     sleep_max = 1
@@ -43,7 +61,7 @@ def set_initial_temp(mini_heater_board,
     mv = 0
     dt = sleep_max
     steps_per_second = int(1 / sleep_max)
-    times, temps, heater_pwms = [], [], []
+    times, temps, heater_pwms, fan_pwms = [], [], [], []
     current_temp = 0
     ind = 0
     while not stable:
@@ -72,6 +90,7 @@ def set_initial_temp(mini_heater_board,
         mv = np.clip(mv, 0, 100)
         mini_heater_board.Q1(mv)
         heater_pwms.append(mv)
+        fan_pwms.append(0)
         temp_array = np.array(temps)
         errors = np.abs(temp_array - temp_sp)
         back_index = int(steps_per_second * hold_time)
@@ -85,4 +104,4 @@ def set_initial_temp(mini_heater_board,
         ind += 1
     mini_heater_board.Q1(0)
     print("Ending set temp procedure, current T = {0} °C".format(current_temp))
-    return times, temps, heater_pwms
+    return times, temps, heater_pwms, fan_pwms
--- code/tclab_runner.py
+++ code/tclab_runner.py
@@ -17,25 +17,66 @@ pntxt2 = "d:{}:o".format(3)
 dpin1 = fan_board.get_pin(pntxt2)
 dpin1.mode = 3
 
-temp_sp = 35
+temp_sp = 50
 tol = 0.1
 hold_time = 30
-file_path = "/data/pid_test(1).csv"
+file_path1 = "/data/pid_test(2).csv"
 folder_path_txt = "hidden/box_folder_path.txt"
 with open(folder_path_txt) as f:
     content = f.readlines()
 content = [x.strip() for x in content]
 box_folder_path = content[0]
-total_file_path = box_folder_path + file_path
+total_file_path1 = box_folder_path + file_path1
 
-times, temps, heater_pwms = set_initial_temp(heater_board,
+times1, temps1, heater_pwms1, fan_pwms1 = set_initial_temp(dpin1,
+                                             heater_board,
                                              temp_sp,
                                              tol,
                                              hold_time,
-                                             file_path=total_file_path)
+                                             file_path=total_file_path1)
+
+temp_sp = 30
+times2, temps2, heater_pwms2, fan_pwms2 = fan_cooling(dpin1,
+                                             heater_board,
+                                             temp_sp)
+temp_sp = 60
+file_path2 = "/data/pid_test(3).csv"
+total_file_path2 = box_folder_path + file_path2
+times3, temps3, heater_pwms3, fan_pwms3 = set_initial_temp(dpin1,
+                                             heater_board,
+                                             temp_sp,
+                                             tol,
+                                             hold_time,
+                                             file_path=total_file_path2)
 heater_board.Q1(0)
 heater_board.Q2(0)
 dpin1.write(0)
 print('Shutting down')
 heater_board.close()
 fan_board.exit()
+
+times, temps, heater_pwms, fan_pwms = [], [], [], []
+times.extend(times1)
+times.extend(times2)
+times.extend(times3)
+temps.extend(temps1)
+temps.extend(temps2)
+temps.extend(temps3)
+heater_pwms.extend(heater_pwms1)
+heater_pwms.extend(heater_pwms2)
+heater_pwms.extend(heater_pwms3)
+fan_pwms.extend(fan_pwms1)
+fan_pwms.extend(fan_pwms2)
+fan_pwms.extend(fan_pwms3)
+df = pd.DataFrame({'time': times,
+                   'temp': temps,
+                   'heater_pwm': heater_pwms,
+                   'fan_pwm': fan_pwms})
+file_path3="/data/heating_cooling(1).csv"
+total_file_path3 = box_folder_path + file_path3
+df.to_csv(total_file_path3)
+
+
+
+
+
