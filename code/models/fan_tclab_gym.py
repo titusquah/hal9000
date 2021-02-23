4b7253b64d4d453798b9b20765a4d58103de16bd
titusquah <46580668+titusquah@users.noreply.github.com>
2021-02-23 16:24:04 -0700

    Update fan_tclab_gym.py

--- code/models/fan_tclab_gym.py
+++ code/models/fan_tclab_gym.py
@@ -266,7 +266,7 @@ class FanTempControlLabBlackBox(gym.Env):
         dth = -c1 * old_dist ** (c2 - 1) * heater_temp \
               + c3 * heater_pwm \
               + c1 * c2 * old_dist ** (c2 - 1) * (
-                          amb_temp - heater_temp) * dist
+                      amb_temp - heater_temp) * dist
         dtc = c4 * heater_temp - c4 * sensor_temp
 
         new_state = np.zeros(2)
@@ -348,16 +348,16 @@ class FanTempControlLabLinearBlackBox(FanTempControlLabBlackBox):
                  c2=0.004,
                  c3=-9.5,
                  c4=0.003):
-        super(FanTempControlLabBlackBox, self).__init__(initial_temp,
-                                                        amb_temp,
-                                                        dt,
-                                                        max_time,
-                                                        d_traj,
-                                                        temp_lb,
-                                                        c1,
-                                                        c2,
-                                                        c3,
-                                                        c4)
+        super(FanTempControlLabLinearBlackBox, self).__init__(initial_temp,
+                                                              amb_temp,
+                                                              dt,
+                                                              max_time,
+                                                              d_traj,
+                                                              temp_lb,
+                                                              c1,
+                                                              c2,
+                                                              c3,
+                                                              c4)
 
     def tclab_step(self, state, time, action, dist, old_dist):
         """
@@ -386,55 +386,56 @@ class FanTempControlLabLinearBlackBox(FanTempControlLabBlackBox):
         return new_state
 
 
-# class FanTempControlLabAdditiveLinear(FanTempControlLabBlackBox):
-#     metadata = {
-#         'render.modes': ['human', 'rgb_array'],
-#         'video.frames_per_second': 30
-#     }
-#
-#     def __init__(self,
-#                  initial_temp=296.15,
-#                  amb_temp=296.15,
-#                  dt=0.1,
-#                  max_time=6000,
-#                  d_traj=None,
-#                  temp_lb=296.15,
-#                  c1=-0.0003,
-#                  c2=0.004,
-#                  c3=-9.5,
-#                  c4=0.003):
-#         super(FanTempControlLabBlackBox, self).__init__(initial_temp,
-#                                                         amb_temp,
-#                                                         dt,
-#                                                         max_time,
-#                                                         d_traj,
-#                                                         temp_lb,
-#                                                         c1,
-#                                                         c2,
-#                                                         c3,
-#                                                         c4)
-#
-#     def tclab_step(self, state, time, action, dist):
-#         """
-#         Function to model TCLab with fan in odeint
-#         :param state: (np.ndarray)
-#         :param time: (np.ndarray) time to integrate function over (s)
-#         :param action: (float) PWM to heater (%)
-#         :param dist: (float) PWM to fan (%)
-#         :return: new_state: (np.ndarray)
-#         """
-#         heater_pwm = action
-#         sensor_temp, heater_temp = state
-#         c1 = self.c1
-#         c2 = self.c2
-#         c3 = self.c3
-#         c4 = self.c4
-#         amb_temp = self.amb_temp
-#
-#         dth = c1 * heater_temp + c2 * heater_pwm + c3 * dist
-#         dtc = c4 * heater_temp - c4 * sensor_temp
-#
-#         new_state = np.zeros(2)
-#         new_state[0] = dtc
-#         new_state[1] = dth
-#         return new_state
+class FanTempControlLabAdditiveLinear(FanTempControlLabBlackBox):
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second': 30
+    }
+
+    def __init__(self,
+                 initial_temp=296.15,
+                 amb_temp=296.15,
+                 dt=0.1,
+                 max_time=6000,
+                 d_traj=None,
+                 temp_lb=296.15,
+                 c1=-0.0003,
+                 c2=0.004,
+                 c3=-9.5,
+                 c4=0.003):
+        super(FanTempControlLabAdditiveLinear, self).__init__(initial_temp,
+                                                              amb_temp,
+                                                              dt,
+                                                              max_time,
+                                                              d_traj,
+                                                              temp_lb,
+                                                              c1,
+                                                              c2,
+                                                              c3,
+                                                              c4)
+
+    def tclab_step(self, state, time, action, dist, old_dist):
+        """
+        Function to model TCLab with fan in odeint
+        :param state: (np.ndarray)
+        :param time: (np.ndarray) time to integrate function over (s)
+        :param action: (float) PWM to heater (%)
+        :param dist: (float) PWM to fan (%)
+        :param old_dist: (float) Last PWM to fan (%) (unused)
+        :return: new_state: (np.ndarray)
+        """
+        heater_pwm = action
+        sensor_temp, heater_temp = state
+        kp = self.c1
+        taup = self.c2
+        kd = self.c3
+        taud = self.c4
+        amb_temp = self.amb_temp
+
+        dtc = (-sensor_temp + kp * heater_pwm) / taup \
+              + (-sensor_temp + kd * dist) / taud
+
+        new_state = np.zeros(1)
+        new_state[0] = dtc
+        new_state[1] = 0
+        return new_state
