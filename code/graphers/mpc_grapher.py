346c39a31af3e7e89349a0af4e37f19a380818ff
titusquah <46580668+titusquah@users.noreply.github.com>
2021-03-11 01:27:54 -0700

    Create mpc_grapher.py

---
+++ code/graphers/mpc_grapher.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+test = 'nominal'
+case = 2
+tlb = 36
+
+file_path = "/data/real_{0}_test_case_{1}.csv".format(test,case)
+folder_path_txt = "../hidden/box_folder_path.txt"
+with open(folder_path_txt) as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+box_folder_path = content[0]
+
+df = pd.read_csv(box_folder_path + file_path)
+
+
+fig, ax = plt.subplots(4)
+ax[0].plot(df.time,
+           df.temp, 'bo', label='Measured', markersize=2)
+ax[0].axhline(tlb, color='b', label='$T_{lb}$')
+ax[0].legend()
+ax[1].plot(df.time,
+           df.fan_pwm, 'b-', label='Fan PWM')
+ax[2].plot(df.time,
+           df.heater_pwm, 'r-', label='Heater PWM')
+ax[3].plot(df.time, df.c1, 'g-', label='$c_1$')
+ax[3].plot(df.time, df.c2, 'r-', label='$c_2$')
+ax[3].plot(df.time, df.c3, 'b-', label='$c_3$')
+ax[3].plot(df.time, df.c4, 'k-', label='$c_4$')
+ax[3].legend()
+plt.show()
